603dce1df336b3a3881419b98bbfc00e16030093
Nhật Tân <khanhu161217@gmail.com>
2025-06-12 17:12:41 +0000

    updated train.sh files

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
-python main.py \
---coco_path data4train/EmoticGender \
---output_dir output \
+python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py \
+--coco_path /kaggle/input/detr-dataset/EmoticGender \
+--output_dir /kaggle/working/output \
 --batch_size 4 \
---epochs 10
+--epochs 10 \
+--resume /kaggle/working/detr-r101-panoptic-40021d53.pth
