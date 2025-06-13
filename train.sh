249df9425d094283957e731fa59c45d25e3ef2d8
Nhật Tân <khanhu161217@gmail.com>
2025-06-13 02:10:51 +0000

    push code

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py \
---coco_path /kaggle/input/detr-dataset/EmoticGender \
---output_dir /kaggle/working/output \
---batch_size 4 \
---epochs 10 \
---resume /kaggle/working/detr-r101-panoptic-40021d53.pth
+    --coco_path /kaggle/input/detr-dataset/EmoticGender \
+    --output_dir /kaggle/working/output \
+    --batch_size 8 \
+    --epochs 10 \
+    --resume /kaggle/working/detr-r101-dc5-a2e86def.pth \
+    --dataset_file emotic \
