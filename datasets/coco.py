9e217f16e010f268dabc33f70d81a230604dbbf6
Nhật Tân <khanhu161217@gmail.com>
2025-07-15 07:32:10 +0000

    push code

--- datasets/coco.py
+++ datasets/coco.py
@@ -159,14 +159,17 @@ def make_coco_transforms(image_set):
 
 def build(image_set, args):
     root = Path(args.coco_path)
-    assert root.exists(), f'provided COCO path {root} does not exist'
-    mode = 'instances'
+    assert root.exists(), f'Provided COCO path {root} does not exist'
+    
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "train2017", root / "annotations" / "instances_train2017.json"),
+        "val": (root / "val2017", root / "annotations" / "instances_val2017.json"),
     }
-
+    
     img_folder, ann_file = PATHS[image_set]
-    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set, args), return_masks=False)
+    assert img_folder.exists(), f'Image folder {img_folder} does not exist'
+    assert ann_file.exists(), f'Annotation file {ann_file} does not exist'
+    
+    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    print(f"Dataset {image_set} initialized with {len(dataset)} images")
     return dataset
-
